da7687d2241755efcfc41873ff48ec0b9d04b0c8
Canleaveye <239064514@qq.com>
2024-10-08 13:52:13 +0800

    Update light.js

--- Oct-vacation/Task4/light.js
+++ Oct-vacation/Task4/light.js
@@ -2,11 +2,11 @@ function changeImage() {
     //1.获取图片对象
     const img = document.querySelector('#myimage');
 //2.判断图片的src属性,off时变为on, on时变为off
-    if ( myimage.src.match("lightoff.gif")) {
-        myimage.src = "lighton.gif";
+    if ( myimage.src.match("./resources/imgs/pic_bulboff.gif")) {
+        myimage.src = "./resources/imgs/pic_bulbon.gif";
     }
     else {
-        myimage.src = "lightoff.gif";
+        myimage.src = "./resources/imgs/pic_bulboff.gif";
     }
     
   }
