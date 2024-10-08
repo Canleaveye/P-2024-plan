ec18b38feb6f2e87621aa38ad5d55e0415e014e5
Canleaveye <239064514@qq.com>
2024-10-08 13:49:25 +0800

    Update light.js

--- Oct-vacation/Task4/light.js
+++ Oct-vacation/Task4/light.js
@@ -1,7 +1,6 @@
 function changeImage() {
     //1.获取图片对象
-    let img = document.getElementById("img");
-    let body = document.getElementById("body");
+    const img = document.querySelector('#myimage');
 //2.判断图片的src属性,off时变为on, on时变为off
     if ( myimage.src.match("lightoff.gif")) {
         myimage.src = "lighton.gif";
